cd25bf4aa2a860fb3073224f38b5c93e1a429ff5
oraz <oraz.mergen@gmail.com>
2020-04-04 20:01:44 +0600

    add urls in project and app. modified model in blog, add get_absolute_url for model

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-def post_least(request):
+def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html',{'posts': posts})
 # Create your views here.
