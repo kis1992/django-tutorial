414b7592485b385936865df731a40dc8e7973621
oraz <oraz.mergen@gmail.com>
2020-04-05 22:30:29 +0600

    create templates for render base,list and detail html files. modified function get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -17,11 +17,11 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-    class Meta:
-        ordering = ('-publish',)
+    #class Meta:
+        #ordering = self.publish('-publish')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year,self.publish.strftime('%m'),self.publish.strftime('%d'),self.slug])
+        return reverse('post_detail', args=[self.publish.year,self.publish.strftime('%m'),self.publish.strftime('%d'),self.slug])
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html',{'posts': posts})
+    posts = Post.objects.order_by('-publish')
+    print(posts)
+    return render(request, 'post/list.html',{'posts': posts})
 # Create your views here.
 
 def post_detail(request,year,month,day,post):
-    post = get_object_or_404(Post, slug = post, status = 'published', publish__year=year, publish__month=month,publish__day = day)
-    return render(request, 'blog/post/detail.html',{'post':post})
+    post = get_object_or_404(Post, slug = post, publish__year=year, publish__month=month,publish__day = day)
+    return render(request, 'post/detail.html',{'post':post})
