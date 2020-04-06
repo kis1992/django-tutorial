104afa7356d515d2bd25d311ec41c30467f935aa
oraz <oraz.mergen@gmail.com>
2020-04-06 23:45:34 +0600

    add meta in class Post to ordering data desc data until publish date

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-    #class Meta:
-        #ordering = self.publish('-publish')
+    class Meta:
+        ordering = ['-publish']
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -4,24 +4,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
 class PostListView(ListView):
-    queryset = Post.objects.order_by('-publish')
+    queryset = Post.objects.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'post/list.html'
-
-def post_list(request):
-    object_list = Post.objects.order_by('-publish')
-    paginator = Paginator(object_list,3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    print(posts)
-    return render(request, 'post/list.html',{'posts': posts})
-# Create your views here.
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post, slug = post, publish__year=year, publish__month=month,publish__day = day)
