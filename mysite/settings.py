c247f246e63a5543cb802cb9676ce647238a9a79
oraz <oraz.mergen@gmail.com>
2020-04-09 22:29:25 +0600

    settings modify

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,10 +28,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-DEFAULT_FROM_EMAIL = 'admin@gmail.com'
-SERVER_EMAIL = 'admin@gmail.com'
-EMAIL_HOST_USER = 'admin@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+DEFAULT_FROM_EMAIL = 'oraz.mergen@gmail.com'
+SERVER_EMAIL = 'oraz.mergen@gmail.com'
+EMAIL_HOST = 'oraz.mergen@gmail.com'
+EMAIL_HOST_USER = 'oraz.mergen@gmail.com'
+EMAIL_HOST_PASSWORD = '14021992mk'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
